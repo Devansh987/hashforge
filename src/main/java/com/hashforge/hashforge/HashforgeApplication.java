20c20e21dd07e4bac8aa5dd7e836b38c1e317fe6
Devansh Tomar <d98291479@gmail.com>
2026-08-08 23:49:43 +0530

    Initial Project Commit

---
+++ src/main/java/com/hashforge/hashforge/HashforgeApplication.java
@@ -0,0 +1,13 @@
+package com.hashforge.hashforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HashforgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HashforgeApplication.class, args);
+	}
+
+}
